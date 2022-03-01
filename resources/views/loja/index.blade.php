6f57644f0cd23c72e75e205cf9713ad01f68be83
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-03-01 14:00:24 -0300

    Reindentação do código

--- resources/views/loja/index.blade.php
+++ resources/views/loja/index.blade.php
@@ -13,19 +13,30 @@
             <h5 class="card-title">Nossas lojas</h5>
             <p class="card-text">Lista de lojas.</p>
             <div class="row">
-                <div class="col-md-8">
-                    <h5>Nome</h5>
+                <div class="col-md-12">
+                    Use o link "http://servidor/loja/id" para retornar um json da loja.
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-1">
+                    <b>ID</b>
+                </div>
+                <div class="col-md-7">
+                    <b>Nome</b>
                 </div>
                 <div class="col-md-1">
-                    <h5>Latitude</h5>
+                    <b>Latitude</b>
                 </div>
                 <div class="col-md-1">
-                    <h5>Longitude</h5>
+                    <b>Longitude</b>
                 </div>
             </div>
             @foreach( $lojas as $loja)
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-1">
+                    {{ $loja->loja_id }}
+                </div>
+                <div class="col-md-7">
                     {{ $loja->nome }}
                 </div>
                 <div class="col-md-1">
@@ -39,36 +50,35 @@
                 </div>
                 <div class="col-md-1">
                     <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-{{ $loja->loja_id  }}">
-                      Excluir
-                  </button>
+                        Excluir
+                    </button>
 
-                  <!-- Modal -->
-                  <div class="modal fade" id="modal-{{ $loja->loja_id  }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                      <div class="modal-dialog">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Excluir</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            Excluir {{ $loja->nome }}
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                            <button type="button" class="btn btn-danger" onclick='$("#{{ $loja->loja_id  }}").submit();'>Deletar</button>
+                    <!-- Modal -->
+                    <div class="modal fade" id="modal-{{ $loja->loja_id  }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <b class="modal-title" id="exampleModalLabel">Excluir</b>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Excluir {{ $loja->nome }}
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                                    <button type="button" class="btn btn-danger" onclick='$("#{{ $loja->loja_id  }}").submit();'>Deletar</button>
+                                </div>
+                            </div>
+                            <form id="{{ $loja->loja_id  }}" action="{{ route ('loja.destroy', $loja->loja_id ) }}" method="post">
+                                {{ csrf_field() }}
+                                <input type='hidden' name='_method' value='DELETE' />
+                            </form>
                         </div>
                     </div>
-                    <form id="{{ $loja->loja_id  }}" action="{{ route ('loja.destroy', $loja->loja_id ) }}" method="post">
-                        {{ csrf_field() }}
-                        <input type='hidden' name='_method' value='DELETE' />
-                    </form>
                 </div>
             </div>
+            @endforeach
         </div>
     </div>
-    @endforeach
-</div>
-</div>
-</div>
 </div>
 @endsection
